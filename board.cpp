bd4b30215ed7981061256e673a9823c4a7a2c7c5
Darius Argueta <71108520+DariusA24@users.noreply.github.com>
2021-12-15 22:40:35 -0500

    update

--- board.cpp
+++ board.cpp
@@ -2,9 +2,13 @@
 #include <iostream>
 #include <vector>
 
+
 //inheritance 
 
 // void board::go(int price, int rent, int location, bool owned){
 //     go.price = 
 // }
 
+board::board(int price, int rent, int location, bool owned){
+    
+}
